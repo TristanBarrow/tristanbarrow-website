4d300d086e992a543933f7d917f389c204814203
Tristan Barrow <tristanmarkbarrow@gmail.com>
2021-03-21 02:54:10 -0600

    small changes

--- gen/index.ts
+++ gen/index.ts
@@ -1,11 +1,14 @@
 import { writeFileSync } from 'fs';
+import config from './config';
 import all from './templates/all.template';
+
+const names = config.map(option => option.name);
 
 let home = `${__dirname}`;
 let homeArray = home.split('/');
 homeArray.pop();
 home = homeArray.join('/')
 
-const allTemplate = all([]);
+const allTemplate = all(names);
 const allLocation = homeArray.concat(allTemplate.location);
 writeFileSync(allLocation.join('/'), allTemplate.template);
--- src/backend/api/endpoints/all.ts
+++ src/backend/api/endpoints/all.ts
@@ -1,11 +1,12 @@
 import { Express } from 'express'; 
 
 import { application } from './application';
-import { todo } from './gen/todo';
 import { user } from './user';
+import { todo } from './gen/todo'
 
 export default (app: Express) => {
     application(app);
     user(app);
     todo(app);
+
 }
